31cd64cfd797a8fc04fef3d30f8da14ca6fb1831
Franco Leyes <franco.leyes@hexium.com.ar>
2024-10-04 00:20:21 -0300

    [IMP] out_of_reality_api: receive video from unity

--- out_of_reality_api/__manifest__.py
+++ out_of_reality_api/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'API',
     'author': 'Franco Leyes - Augusto cáceres - Santiago Agüero',
     'license': 'LGPL-3',
-    'depends': ['fastapi', 'auth_faceid'],
+    'depends': ['fastapi', 'auth_faceid', 'clinic_management'],
     'data': [
         'data/api_data.xml',
         'security/security.xml',
