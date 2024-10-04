38db64ed3c943437519d58baaa5fab97cbf58bbe
Franco Leyes <franco.leyes@hexium.com.ar>
2024-10-04 00:20:21 -0300

    [IMP] out_of_reality_api: receive video from unity
    Part-of: out-of-reality/out-of-reality#6
    Signed-off-by: Franco Leyes <leyes404@gmail.com>

--- out_of_reality_api/routers/__init__.py
+++ out_of_reality_api/routers/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .users import router as users_router
+from .videos import router as videos_router
 
 router = APIRouter()
 router.include_router(users_router)
+router.include_router(videos_router)
