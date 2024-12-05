b019e2a1256b022e5344a13f1c7f65bd4e27406a
Franco Leyes <franco.leyes@hexium.com.ar>
2024-12-04 21:54:12 -0300

    [TEST] not merge

--- clinic_management/models/clinic_game_session.py
+++ clinic_management/models/clinic_game_session.py
@@ -10,3 +10,7 @@ class ClinicGameSession(models.Model):
     video = fields.Binary()
     filename = fields.Char()
     session_date = fields.Date(default=fields.Datetime.now)
+    test_field = fields.Char()
+
+
+
