3fdda980e08ff5bc82a04715e405cc601dfecb38
Franco Leyes <franco.leyes@hexium.com.ar>
2024-12-28 16:32:20 -0300

    [ADD] auth_faceid: add new module
    closes out-of-reality/out-of-reality#16
    
    Signed-off-by: Franco Leyes <leyes404@gmail.com>

---
+++ auth_faceid/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Auth FaceID",
+    "version": "17.0.1.0.0",
+    "author": "Out of reality",
+    "license": "LGPL-3",
+    "website": "https://github.com/out-of-reality/out-of-reality",
+    "depends": ["web"],
+    "external_dependencies": {"python": ["face_recognition", "python-jose"]},
+    "data": [
+        "views/login_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "assets": {
+        "web.assets_frontend": [
+            "auth_faceid/static/src/login_faceid/**/*",
+        ],
+    },
+}
