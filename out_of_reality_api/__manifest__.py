ab8c03e5a15ac91162933119333dcf05b9236160
staguero <143agueros@gmail.com>
2025-01-29 19:41:55 -0300

    rep

--- out_of_reality_api/__manifest__.py
+++ out_of_reality_api/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Out of reality",
     "license": "LGPL-3",
     "website": "https://github.com/out-of-reality/out-of-reality",
-    "depends": ["fastapi", "auth_faceid", 'clinic_management'],
+    "depends": ["fastapi", "auth_faceid"],
     "data": [
         "data/api_data.xml",
         "security/security.xml",
